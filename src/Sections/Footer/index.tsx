0a6be4113445e4d0c91efab08966e1b8b2e69372
revione <revione@outlook.com>
2023-11-01 21:58:33 +0100

    Footer

--- src/Sections/Footer/index.tsx
+++ src/Sections/Footer/index.tsx
@@ -1,5 +1,35 @@
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>Linea</div>
+      <div>
+        <div>Imagen</div>
+        <div>
+          <div>Secciones</div>
+          <ul>
+            <li>Inicio</li>
+            <li>Método</li>
+            <li>Planes</li>
+            <li>Interactuemos</li>
+          </ul>
+        </div>
+        <div>
+          <div>Contacto</div>
+          <ul>
+            <li>+34657241310</li>
+            <li>tucoach@conciencia11.com</li>
+            <li>
+              <div>Linkedin</div>
+              <div>Instagram</div>
+              <div>facebook</div>
+              <div>Tiktok</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>©2023 Conecta tu propósito. All rights reserved</div>
+    </div>
+  );
 };
 
 export default Footer;
