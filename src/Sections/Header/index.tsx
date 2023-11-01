e16c85b544fc542ef2b8ee4836eb4e659d20715b
revione <revione@outlook.com>
2023-11-01 22:00:33 +0100

    Header

--- src/Sections/Header/index.tsx
+++ src/Sections/Header/index.tsx
@@ -1,5 +1,26 @@
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>Logo</div>
+        <div>
+          <div>Linkedin</div>
+          <div>Instagram</div>
+          <div>facebook</div>
+          <div>Tiktok</div>
+        </div>
+      </div>
+
+      <div>
+        <div>Inicio</div>
+        <div>Método</div>
+        <div>Planes</div>
+        <div>Interactuemos</div>
+        <div>Blog</div>
+        <div>Asesoria - Free</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
