4a16c0e764d3e10ce3b034d15a37f6e606a2ec54
revione <revione@outlook.com>
2023-11-01 21:43:23 +0100

    Section4

--- src/Sections/4/index.tsx
+++ src/Sections/4/index.tsx
@@ -1,5 +1,57 @@
+const CardsInfo = [
+  {
+    imgSrc: "url",
+    title: "",
+    content: "",
+  },
+  {
+    imgSrc: "url",
+    title: "",
+    content: "",
+  },
+  {
+    imgSrc: "url",
+    title: "",
+    content: "",
+  },
+  {
+    imgSrc: "url",
+    title: "",
+    content: "",
+  },
+  {
+    imgSrc: "url",
+    title: "",
+    content: "",
+  },
+];
+
+const Card = () => {
+  return (
+    <div>
+      <div>Background</div>
+      <div>
+        <div>Title</div>
+        <div>Content</div>
+      </div>
+    </div>
+  );
+};
+
 const Section4 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>Método de transformación</div>
+        <div>enfocado en descubrir:</div>
+      </div>
+      <div>
+        {CardsInfo.map((_data, index) => (
+          <Card key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Section4;
