dacacb4fdd1d020aa9c2cc73ce3014e465c3a0a2
revione <revione@outlook.com>
2023-11-01 21:55:22 +0100

    Section6

--- src/Sections/6/index.tsx
+++ src/Sections/6/index.tsx
@@ -1,5 +1,32 @@
+const CardsInformation = [
+  {
+    content: "content",
+    name: "name",
+    profesion: "profesion",
+  },
+];
+
+const Card = ({ content, name, profesion }: any) => {
+  return (
+    <div>
+      <div>{content}</div>
+      <div>{name}</div>
+      <div>{profesion}</div>
+    </div>
+  );
+};
+
 const Section6 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>Transformaciones</div>
+      <div>
+        {CardsInformation.map((_data, index) => (
+          <Card key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Section6;
