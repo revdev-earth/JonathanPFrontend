3156994a6cf0a572563cd287a84e8fd9afef6bec
revione <revione@outlook.com>
2023-11-01 21:34:07 +0100

    Section2

--- src/Sections/2/index.tsx
+++ src/Sections/2/index.tsx
@@ -1,5 +1,30 @@
 const Section2 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          ¡¡ Toma acción y responsabilidad, no es casualidad que hayas llegado
+          hasta aquí!!
+        </div>
+        <div>
+          <div>
+            <div>Querido viajero y sembrador en el tiempo,</div>
+            <div>
+              Si sientes que ya es hora de que despiertes a tu verdadero
+              potencial, que te permitas florecer en la plenitud de tu ser, que
+              descubras tu propósito y vayas en búsqueda de tu mejor versión.
+              Escoge el plan que mejor se adapte a tu necesidad y empecemos a
+              darle un cambio radical a tu vida. O si todavía no te sientes
+              seguro de tu decisión, agenda una charla conmigo y exploremos
+              juntos que es lo que no te permite iniciar.
+            </div>
+            <div>QUIERO VER LOS PLANES</div>
+          </div>
+          <div>Calendly</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Section2;
