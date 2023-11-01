f88693303410e893dbbc7485e2668d043a4f688f
revione <revione@outlook.com>
2023-11-01 21:39:03 +0100

    Section3

--- src/Sections/3/index.tsx
+++ src/Sections/3/index.tsx
@@ -1,5 +1,63 @@
 const Section3 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="absolute">
+        <div>Img background 1</div>
+        <div>Img background 2</div>
+      </div>
+      <div>
+        <div>
+          <div>Emprendedor digital, </div>
+          <div>Especialista en Life Coaching,</div>
+          <div>Proyector de Vida, Formador.</div>
+        </div>
+        <div>
+          <div>
+            <div>Image</div>
+            <div>
+              "Quiero a través de mis redes sociales talleres, conferencias,
+              sesiones personalizadas, sembrar la consciencia en el mundo,
+              guiado por el amor, la ética y la coherencia. Aspiro a tocar las
+              vidas de las personas y acompañarlas en sus procesos vitales,
+              reconociendo nuestra existencia en este mundo como una experiencia
+              humana llena de desafíos y oportunidades que podemos explorar
+              juntos. En este viaje, mi propósito es analizar cada aspecto de la
+              vida desde la amplitud de nuestra existencia, reconociendo la
+              integridad de cuerpo, alma y mente que nos define como seres
+              perfectos en evolución."
+            </div>
+          </div>
+
+          <div>
+            <div>Image</div>
+            <div>
+              <ul>
+                <li>
+                  • Al igual que la fotosíntesis, el crecimiento personal es un
+                  proceso que requiere tiempo, dedicación у paciencia.{" "}
+                </li>
+                <li>
+                  • Al igual que las plantas necesitan luz para realizar la
+                  fotosíntesis, las personas necesitan aprender y experimentar
+                  cosas nuevas para crecer.
+                </li>
+                <li>
+                  • Al igual que las plantas necesitan nutrientes para crecer,
+                  las personas necesitan rodearse de personas positivas y tener
+                  una actitud positiva para crecer.
+                </li>
+                <li>
+                  • Al igual que las plantas necesitan agua para sobrevivir, las
+                  personas necesitan cuidar su cuerpo y su mente para
+                  sobrevivir.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Section3;
