5c03e9912ef83d10ddeaf2a8461e1ebc4d926da5
revione <revione@outlook.com>
2023-11-01 21:31:51 +0100

    Section1

--- src/Sections/1/index.tsx
+++ src/Sections/1/index.tsx
@@ -1,5 +1,16 @@
 const Section1 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>Logo</div>
+      <div>¡Descubre tu resplandor!</div>
+      <div>
+        A través de este enfoque innovador y revolucionario que te acompañara
+        hacia la revelación de tu luz interior y desbloqueara el potencial
+        oculto de tu ser para que disfrutes de una vida con propósito.
+      </div>
+      <div>INVERTIR EN MI CRECIMIENTO</div>
+    </div>
+  );
 };
 
 export default Section1;
