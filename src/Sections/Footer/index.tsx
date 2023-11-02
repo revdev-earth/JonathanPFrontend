5320c1ce61882df6a5cf770ce8b8178d201b03e4
revione <revione@outlook.com>
2023-11-02 21:34:27 +0100

    Footer

--- src/Sections/Footer/index.tsx
+++ src/Sections/Footer/index.tsx
@@ -2,9 +2,15 @@ const Footer = () => {
   return (
     <div>
       <div>Linea</div>
-      <div>
-        <div>Imagen</div>
-        <div>
+      <div className="h-[260px] bg-[#092435] flex justify-between px-20">
+        <div className="flex justify-center items-center">
+          <img
+            src="src/assets/footer/logo-footer.png"
+            alt="logo footer"
+            className="w-[487px] h-[152px]"
+          />
+        </div>
+        <div className="flex flex-col justify-center">
           <div>Secciones</div>
           <ul>
             <li>Inicio</li>
@@ -13,7 +19,7 @@ const Footer = () => {
             <li>Interactuemos</li>
           </ul>
         </div>
-        <div>
+        <div className="flex flex-col justify-center">
           <div>Contacto</div>
           <ul>
             <li>+34657241310</li>
@@ -27,7 +33,9 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div>©2023 Conecta tu propósito. All rights reserved</div>
+      <div className="h-[80px] bg-gradient-to-r from-cyan-500 to-blue-500">
+        ©2023 Conecta tu propósito. All rights reserved
+      </div>
     </div>
   );
 };
