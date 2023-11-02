f12046b6946fa27044f9e8b5f173efe6f59fd354
revione <revione@outlook.com>
2023-11-02 10:11:27 +0100

    section3

--- src/Sections/3/index.tsx
+++ src/Sections/3/index.tsx
@@ -1,19 +1,48 @@
 const Section3 = () => {
   return (
-    <div>
-      <div className="absolute">
-        <div>Img background 1</div>
-        <div>Img background 2</div>
-      </div>
+    <div className="min-h-[800px] bg-[#092435] px-24 py-12 relative">
       <div>
-        <div>
+        <img
+          src="src/assets/section3/vector-izquierda-section-3.png"
+          alt="logo"
+          className="
+            absolute left-0 top-0
+                          w-[180px] max-w-none
+            min-h-[210px] h-[213px] 
+          "
+        />
+
+        <img
+          src="src/assets/section3/vector-derecha-section-3.png"
+          alt="logo"
+          className="
+            absolute right-0 top-0
+                          w-[200px] max-w-none
+            min-h-[164px] h-[164px] 
+          "
+        />
+      </div>
+      <div className="flex flex-col gap-12 text-white">
+        <div className="flex flex-col text-white items-center h-[97px]">
           <div>Emprendedor digital, </div>
           <div>Especialista en Life Coaching,</div>
           <div>Proyector de Vida, Formador.</div>
         </div>
-        <div>
-          <div>
-            <div>Image</div>
+
+        <div className="flex flex-col gap-12">
+          <div className="flex gap-10">
+            <div
+              className="
+                flex justify-center items-center
+                min-w-[400px] min-h-[200px] bg-[#FFFAEB] rounded-lg
+              "
+            >
+              <img
+                src="src/assets/section3/logo-section-3.png"
+                alt="logo"
+                className="max-w-none w-[300px] min-h-[100px] h-[100px] "
+              />
+            </div>
             <div>
               "Quiero a través de mis redes sociales talleres, conferencias,
               sesiones personalizadas, sembrar la consciencia en el mundo,
@@ -28,8 +57,19 @@ const Section3 = () => {
             </div>
           </div>
 
-          <div>
-            <div>Image</div>
+          <div className="flex gap-10">
+            <div
+              className="
+                flex justify-center items-center
+                min-w-[400px] min-h-[200px] bg-[#FFFAEB] rounded-lg
+              "
+            >
+              <img
+                src="src/assets/section3/logo-fotosintesis-section-3.png"
+                alt="logo"
+                className="max-w-none w-[300px] min-h-[100px] h-[100px] "
+              />
+            </div>
             <div>
               <ul>
                 <li>
