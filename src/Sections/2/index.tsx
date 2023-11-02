364d95eab0320fd6b2a1549753679ec0c91d00fb
Nickothan <s1nicolasrg@gmail.com>
2023-11-02 13:29:26 -0500

    Avance section2.

--- src/Sections/2/index.tsx
+++ src/Sections/2/index.tsx
@@ -1,24 +1,26 @@
 const Section2 = () => {
   return (
-    <div className=" p-24">
+    <div className=" p-24 pt-32">
       <div>
-        <div className="flex bg-[#092435] font-bold rounded-xl p-8 pt-16 gap-4 text-white relative">
+        <div className="relative flex gap-4 ml-2 pl-12 pt-[76px] w-[1237px] h-[600px] bg-[#092435] text-white font-bold rounded-2xl">
           <div
             className="
-              flex items-center
-              font-bold rounded-xl
-              h-16 w-max px-10
-              bg-gradient-to-r from-cyan-500 to-blue-500
               absolute
               -top-8 left-2/4 -translate-x-2/4
+              flex items-center
+              h-16 w-max px-10
+              bg-gradient-to-r from-cyan-500 to-blue-500
+              font-bold rounded-xl text-2xl text-[#092435]
           "
           >
             ¡¡ Toma acción y responsabilidad, no es casualidad que hayas llegado
             hasta aquí!!
           </div>
-          <div className="flex flex-col gap-2 w-2/4">
-            <div>Querido viajero y sembrador en el tiempo,</div>
-            <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-6 w-2/4">
+            <div className="w-[500px]  text-[42px] font-bold leading-[54px]">
+              Querido viajero y sembrador en el tiempo,
+            </div>
+            <div className="flex flex-col gap-4 w-[500px] text-[18px] leading-[25px] font-normal">
               <p>
                 Si sientes que ya es hora de que despiertes a tu verdadero
                 potencial, que te permitas florecer en la plenitud de tu ser,
@@ -36,7 +38,7 @@ const Section2 = () => {
               </p>
             </div>
             <div>
-              <button className="flex items-center uppercase px-4 h-10 border-2 rounded-md border-[#2ABA64]">
+              <button className="flex items-center uppercase mt-[7px] px-4 h-10 border-2 rounded-md border-[#2ABA64]">
                 QUIERO VER LOS PLANES
               </button>
             </div>
