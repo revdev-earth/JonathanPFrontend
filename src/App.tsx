96118b8444922bbb730e147693eb66f15915e6f3
Nickothan <s1nicolasrg@gmail.com>
2023-11-02 13:10:25 -0500

    pixel perfect section 1 home.

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,15 @@ import Footer from "Sections/Footer";
 function App() {
   return (
     <>
+      <div className="m-auto max-w-[1440px]">
       <Header />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Section6 />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Section6 />
+      </div>
       <Footer />
     </>
   );
--- src/Sections/1/index.tsx
+++ src/Sections/1/index.tsx
@@ -3,24 +3,40 @@ import LogotFotosintesis from "/section1/logo-fotosintesis.png";
 const Section1 = () => {
   return (
     <div
-      className="flex items-center
-        min-h-[500px] px-24 relative
-        bg-[url('/section1/hero-bg.png')]
+      className="flex relative
+        min-h-[577px] px-24 pt-[72px]
+        bg-[url('src/assets/hero/hero-bg.png')]
+        border-b-[10px] 
+        after:content-['*'] after:absolute 
+        after:bottom-[-10px] after:left-0 
+        after:h-[10px] after:w-full
+        after:bg-gradient-to-r after:from-[#2ABA64] after:to-[#3369FF]
         "
     >
       <img src="/section1/rama.png" alt="rama" className="absolute right-0" />
 
-      <div className="flex flex-col gap-2 max-w-[750px]">
+      <div className="flex flex-col gap-6">
         <div>
           <img src={LogotFotosintesis} alt="logo fotosintesis" />
         </div>
-        <div>¡Descubre tu resplandor!</div>
-        <div>
-          A través de este enfoque innovador y revolucionario que te acompañara
-          hacia la revelación de tu luz interior y desbloqueara el potencial
-          oculto de tu ser para que disfrutes de una vida con propósito.
+
+        <div className="flex flex-col gap-4">
+          <div className=" text-5xl font-bold">¡Descubre tu resplandor!</div>
+
+          <div className=" text-2xl font-semibold leading-[30PX] max-w-[785px]">
+            A través de este enfoque innovador y revolucionario que te
+            acompañara hacia la revelación de tu luz interior y desbloqueara el
+            potencial oculto de tu ser para que disfrutes de una vida con
+            propósito.
+          </div>
         </div>
-        <div>INVERTIR EN MI CRECIMIENTO</div>
+        <button
+          className="bg-[#2ABA64] w-[334px] mt-2 p-1
+                            font-bold text-base rounded-md
+                            shadow-[0_0px_6px_0px_rgba(0,0,0)]"
+        >
+          INVERTIR EN MI CRECIMIENTO
+        </button>
       </div>
     </div>
   );
