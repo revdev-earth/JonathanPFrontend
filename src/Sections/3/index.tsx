1466a08f77320bc0e46408992b59e4177ef2fe49
Nickothan <s1nicolasrg@gmail.com>
2023-11-03 08:11:33 -0500

    Section 3.

--- src/Sections/3/index.tsx
+++ src/Sections/3/index.tsx
@@ -1,32 +1,36 @@
 const Section3 = () => {
   return (
-    <div className="min-h-[800px] bg-[#092435] px-24 py-12 relative">
+    <div className="min-h-[888px] bg-[#092435] px-28 pt-14 pb- relative">
       <div>
         <img
           src="/section3/vector-izquierda-section-3.png"
           alt="logo"
           className="
             absolute left-0 top-0
-                          w-[180px] max-w-none
-            min-h-[210px] h-[213px] 
+                          w-[255px] max-w-none
+            min-h-[210px] h-[325px] 
           "
         />
-
         <img
           src="/section3/vector-derecha-section-3.png"
           alt="logo"
           className="
             absolute right-0 top-0
-                          w-[200px] max-w-none
-            min-h-[164px] h-[164px] 
+                          w-[280px] max-w-none
+            min-h-[164px] h-[222px] 
           "
         />
       </div>
-      <div className="flex flex-col gap-12 text-white">
-        <div className="flex flex-col text-white items-center h-[97px]">
-          <div>Emprendedor digital, </div>
-          <div>Especialista en Life Coaching,</div>
-          <div>Proyector de Vida, Formador.</div>
+      <div className="flex flex-col justify-center items-center gap-16 w-[1200px] text-white">
+        <div className="flex flex-col text-center items-center  
+                      w-[600px]  text-[42px] font-bold leading-[45px]
+                      bg-clip-text         
+                      text-transparent  bg-gradient-to-r from-[#2ABA64] to-[#3369FF]
+        
+        ">
+          Emprendedor digital, 
+          Especialista en Life Coaching,
+          Proyector de Vida, Formador.
         </div>
 
         <div className="flex flex-col gap-12">
@@ -34,7 +38,7 @@ const Section3 = () => {
             <div
               className="
                 flex justify-center items-center
-                min-w-[400px] min-h-[200px] bg-[#FFFAEB] rounded-lg
+                min-w-[400px]  min-h-[188px] bg-[#FFFAEB] rounded-lg
               "
             >
               <img
@@ -43,7 +47,7 @@ const Section3 = () => {
                 className="max-w-none w-[300px] min-h-[100px] h-[100px] "
               />
             </div>
-            <div>
+            <div className="text-lg leading-[25px] font-normal">
               "Quiero a través de mis redes sociales talleres, conferencias,
               sesiones personalizadas, sembrar la consciencia en el mundo,
               guiado por el amor, la ética y la coherencia. Aspiro a tocar las
@@ -70,11 +74,11 @@ const Section3 = () => {
                 className="max-w-none w-[300px] min-h-[100px] h-[100px] "
               />
             </div>
-            <div>
+            <div className="text-lg leading-[25px] font-normal">
               <ul>
                 <li>
                   • Al igual que la fotosíntesis, el crecimiento personal es un
-                  proceso que requiere tiempo, dedicación у paciencia.{" "}
+                  proceso que requiere tiempo, dedicación у paciencia.
                 </li>
                 <li>
                   • Al igual que las plantas necesitan luz para realizar la
@@ -95,6 +99,14 @@ const Section3 = () => {
             </div>
           </div>
         </div>
+
+          <button className="flex items-center 
+                                mt-[7px] px-4 h-10 w-[]
+                                uppercase text-base font-bold 
+                                tracking-[.45em] leading-4
+                                border-2 rounded-md border-[#2ABA64]">
+                VER LOS PLANES AHORA
+              </button>
       </div>
     </div>
   );
