bd83b7aeb73296b0e9c77cb9784520aa6d9ed0a7
Nickothan <s1nicolasrg@gmail.com>
2023-11-03 10:29:19 -0500

    Header movile.

--- src/Sections/Header/index.tsx
+++ src/Sections/Header/index.tsx
@@ -1,19 +1,25 @@
 const Header = () => {
   return (
-    <div className="flex bg-[#092435] h-[120px] justify-between px-24">
-      <div className="flex items-center gap-8">
-        <div>
+    <div className="flex items-center  justify-between
+                  h-[88px] px-[20px]
+                  lg:h-[120px]  lg:px-24
+                  bg-[#092435]">
+      <div className="flex items-center gap-[18px]">
+        <div className="w-[111px] h-[69px]">
           <img src="/header/logo.png" alt="logo" />
         </div>
-        <div className="flex gap-5 ">
+        <div className="flex gap-[13px] h-[13px]">
           <img src="/header/linkedin.png" alt="Linkedin" />
           <img src="/header/instagram.png" alt="Instagram" />
           <img src="/header/facebook.png" alt="facebook" />
           <img src="/header/tiktok.png" alt="Tiktok" />
         </div>
       </div>
+        <div className="">
+          <img src="/header/hamburger.png" alt="Tiktok" />
+        </div>
 
-      <div className="flex text-white gap-7 items-center">
+      <div className="hidden text-white gap-7 items-center">
         <div>Inicio</div>
         <div>Método</div>
         <div>Planes</div>
