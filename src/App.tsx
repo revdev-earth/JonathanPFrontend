89c46dd23ff6f2eeae0389c186f191ddf2270143
Nickothan <s1nicolasrg@gmail.com>
2023-11-03 12:01:26 -0500

    descomentar sections.

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ function App() {
     <>
       <Header />
       <Section1 />
-{/*       <Section2 />
+      <Section2 />
       <Section3 />
       <Section4 />
       <Section5 />
       <Section6 />
-      <Footer />   */}
+      <Footer />   
       
     </>
   );
