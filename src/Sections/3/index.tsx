5c4a9318960352af0a31da2a75a4acfd9ddf18eb
Nickothan <s1nicolasrg@gmail.com>
2023-11-04 13:06:18 -0500

    section3 desktop.

--- src/Sections/3/index.tsx
+++ src/Sections/3/index.tsx
@@ -1,14 +1,14 @@
 const Section3 = () => {
   return (
-    <div className="m-5 mt-8 md:min-h-[800px] bg-[#092435] md:px-24 rounded-lg py-12 pb-8 relative">
+    <div className="m-5 md:m-0 mt-8 md:min-h-[800px] bg-[#092435] md:px-24 rounded-lg md:rounded-none py-12 md:py-16  md:pb-8 relative">
       <div>
         <img
           src="/section3/vector-izquierda-section-3.png"
           alt="logo"
           className="
             absolute left-0 top-0
-            w-[58px] h-[81px] md:w-[180px] max-w-none
-            md:min-h-[210px] md:h-[213px] 
+            w-[58px] h-[81px] md:w-[255px] max-w-none
+            md:min-h-[210px] md:h-[326px] 
           "
         />
 
@@ -17,34 +17,34 @@ const Section3 = () => {
           alt="logo"
           className="
             absolute right-0 top-1 md:top-0
-            w-[58px] h-[55px] md:w-[200px] max-w-none
-            md:min-h-[164px] md:h-[164px] 
+            w-[58px] h-[55px] md:w-[279px] max-w-none
+            md:min-h-[164px] md:h-[224px] 
           "
         />
       </div>
-      <div className="flex flex-col items-center  gap-6 md:gap-12 text-white ">
-        <div className="flex flex-col text-white text-center md:items-center md:h-[97px] text-xl leading-[22px] font-gopher font-bold">
+      <div className="flex flex-col items-center  gap-6 md:gap-12 md:pt-0 text-white ">
+        <div className="flex flex-col text-white text-center md:items-center md:h-[97px] text-xl md:text-[42px] leading-[22px] md:leading-[45px] font-gopher font-bold">
           <div>Emprendedor digital, </div>
           <div>Especialista en Life Coaching,</div>
           <div>Proyector de Vida, Formador.</div>
         </div>
 
-        <div className="flex flex-col items-center gap-10 md:gap-12 ">
-          <div className="flex flex-col items-center md:flex-row gap-2 md:gap-10 box-content px-5 text-base font-normal font-nunito leading-[21px]">
+        <div className="flex flex-col items-center gap-10 md:gap-12 md:pl-[22px] md:pt-12">
+          <div className="flex flex-col items-center md:items-start md:flex-row gap-2 md:gap-10 box-content px-5 text-base font-normal font-nunito leading-[21px] ">
             <div
               className="
                 flex  justify-center items-center box-content
-                w-[206px] h-[97px] 
-                md:min-w-[400px] md:min-h-[200px] bg-[#FFFAEB] rounded-lg
+                w-[206px] h-[97px] md:mt-1
+                md:min-w-[400px] md:min-h-[188px] bg-[#FFFAEB] rounded-lg
               "
             >
               <img
                 src="/section3/logo-section-3.png"
                 alt="logo"
-                className="max-w-none w-[150px] md:w-[300px] md:min-h-[100px] h-[51px] md:h-[100px] "
+                className="max-w-none w-[150px] md:w-[290px] md:min-h-[100px] h-[51px] md:h-[100px] "
               />
             </div>
-            <div>
+            <div className="md:text-lg md:leading-[25px]">
               "Quiero a través de mis redes sociales talleres, conferencias,
               sesiones personalizadas, sembrar la consciencia en el mundo,
               guiado por el amor, la ética y la coherencia. Aspiro a tocar las
@@ -58,21 +58,21 @@ const Section3 = () => {
             </div>
           </div>
 
-          <div className="flex flex-col items-center md:flex-row gap-3 md:gap-10 box-content px-5 pr-2 text-base font-normal font-nunito leading-[21px]">
+          <div className="flex flex-col items-center md:flex-row gap-3 md:gap-7 box-content px-5 md:px-[18px] pr-2 text-base font-normal font-nunito leading-[21px]">
             <div
               className="
-                flex justify-center items-center mr-3
-                w-[223px] h-[122px] md:min-w-[400px] md:min-h-[200px] bg-[#FFFAEB] rounded-lg
+                flex justify-center items-center mr-3 md:mt-2
+                w-[223px] h-[122px] md:min-w-[399px] md:min-h-[218px] bg-[#FFFAEB] rounded-lg
               "
             >
               <img
                 src="/section3/logo-fotosintesis-section-3.png"
                 alt="logo"
-                className="max-w-none w-[176px] md:w-[300px] md:min-h-[100px] h-[56px] md:h-[100px] "
+                className="max-w-none w-[176px] md:w-[316px] md:min-h-[100px] h-[56px] md:h-[100px] "
               />
             </div>
-            <div>
-              <ul className="flex flex-col gap-[10px]">
+            <div className="md:text-lg md:leading-[25px] ">
+              <ul className="flex flex-col gap-[10px] md:mt-2">
                 <li>
                   • Al igual que la fotosíntesis, el crecimiento personal es un
                   proceso que requiere tiempo, dedicación у paciencia.{" "}
@@ -96,7 +96,7 @@ const Section3 = () => {
             </div>
           </div>
         </div>
-        <button className="inline-block uppercase mx-4 md:m-0  md:w-auto px-4 h-10 text-center border-2 rounded-md border-[#2ABA64] tracking-[.15em]">
+        <button className="inline-block uppercase mx-4 md:m-0  md:w-auto px-4 h-10 text-center md:text-base font-nunito border-2 rounded-md border-[#2ABA64] tracking-[.35em]">
           VER PLANES AHORA
         </button>
                 
