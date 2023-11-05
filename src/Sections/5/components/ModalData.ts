5820411fa38fcc5836ff49ea6599b32c0de0d20b
Nickothan <s1nicolasrg@gmail.com>
2023-11-04 19:10:24 -0500

    Datos de popups.

--- src/Sections/5/components/ModalData.ts
+++ src/Sections/5/components/ModalData.ts
@@ -57,4 +57,81 @@ export const ModalData = [
     text3:
       "¡¡ Toma acción y responsabilidad, no es casualidad que hayas llegado hasta aquí!!",
   },
+  {
+    title: "Recursos Ilimitados",
+    price: 597,
+    priceText: "Programa de 6 Sesiones",
+    subtitle: "Estas transformaciones se realizan...",
+    text1: [
+      "Por medio de una Sesión online personalizada, de entre 60 a 90 minutos, en una videoconferencia en la plataforma Meet donde podrás elegir el horario que te quede más asequible, recuerda activar tu cámara y audio, lo podrás realizar desde cualquier parte del mundo.",
+      "Necesitamos un espacio seguro para ti, sin interrupciones, que te dé completa tranquilidad y seguridad para comunicarte.",
+    ],
+    question: "¿Qué obtienes?",
+    answer:
+      "6 videollamadas privadas de entre 60 y 90 minutos con Jonathan Pérez. Las llamadas serán semanales o cada dos semanas.",
+    text2:
+      "Bienvenido a nuestro excepcional Método de transformación, Un enfoque integral te brindará las herramientas necesarias para superar obstáculos y tomar medidas decisivas, allanando el camino hacia el éxito y la abundancia. ¡No te conformes con menos de lo que mereces!",
+    preferentWords: {
+      blue: ["las herramientas"],
+      green: ["la abundancia"],
+    },
+    titleList1: "Si te sientes así...",
+    list1: [
+      "Sientes que gastas más de lo que ganas.",
+      "Sientes bloqueos con respecto al dinero y no sabes cómo superarlos.",
+      "Sientes que tu relación con el dinero no es buena.",
+      "No tienes metas, ni objetivos financieros.",
+      "No sabes cuales son los mecanismos y las herramientas para una vida más prospera y abundante.",
+    ],
+    titleList2: "Te acompañare a...",
+    list2: [
+      "Tendrías la capacidad de viajar a cualquier lugar en el planeta.",
+      "Tendrías prosperidad y abundancia en todos los aspectos de tu vida.",
+      "Podrías compartir y pasar tiempo con las personas que amas.",
+      "Te comprarías la casa que siempre has soñado.",
+    ],
+    text3:
+      "¡¡ Toma acción y responsabilidad, no es casualidad que hayas llegado hasta aquí!!",
+  },
+  {
+    title: "Fotosíntesis Humana",
+    price: 897,
+    priceText: "Programa de 10 Sesiones",
+    subtitle: "Estas transformaciones se realizan...",
+    text1: [
+      "Por medio de una Sesión online personalizada, de entre 60 a 90 minutos, en una videoconferencia en la plataforma Meet donde podrás elegir el horario que te quede más asequible, recuerda activar tu cámara y audio, lo podrás realizar desde cualquier parte del mundo.",
+      "Necesitamos un espacio seguro para ti, sin interrupciones, que te dé completa tranquilidad y seguridad para comunicarte.",
+    ],
+    question: "¿Qué obtienes?",
+    answer:
+      "10 videollamadas privadas de entre 60 y 90 minutos con Jonathan Pérez. Las llamadas serán semanales o cada dos semanas.",
+    text2:
+      "El método de transformación y acompañamiento que te guiara a encontrarte contigo mismo, a realizar esa transformación que tanto necesitas, explorando cuál es la verdad sobre ti mismo, Es hora de que despiertes a tu verdadero potencial, que te permitas florecer en la plenitud de tu ser.",
+    preferentWords: {
+      blue: ["acompañamiento", "plenitud de tu ser."],
+      green: ["tu verdadero potencial."],
+    },
+    titleList1: "Si te sientes así...",
+    list1: [
+      "Te sientes con baja Autoestima y Amor Propio.",
+      "Quieres cambiar tu vida, pero sientes miedo al fracaso y al cambio.",
+      "Sientes que no encajas en ningún lugar y es mejor estar solo.",
+      "Quieres tener la vida de tus sueños, pero no sabes por dónde empezar.",
+      "Llevas tiempo haciendo mil cosas, pero sigues obteniendo los mismos resultados.",
+      "Te sientes con muchos bloqueos y limitaciones en tu vida.",
+      "Te encuentras en una zona de confort y no sabes cómo salir de ella."
+    ],
+    titleList2: "Te acompañare a...",
+    list2: [
+      "Tener más autoestima y amor propio.",
+      "Ser tu mejor versión y tener hábitos saludables.",
+      "Vivir de acuerdo a tu esencia, disfrutando de cada paso.",
+      "Tener más confianza en ti mismo para afrontar cualquier reto en tu vida.",
+      "Pasar de una situación actual a una situación deseada.",
+      "Facilitar tu crecimiento y la toma de conciencia.",
+      "Hacerte responsable de tu vida y tomar acción."
+    ],
+    text3:
+      "¡¡ Toma acción y responsabilidad, no es casualidad que hayas llegado hasta aquí!!",
+  },
 ];
