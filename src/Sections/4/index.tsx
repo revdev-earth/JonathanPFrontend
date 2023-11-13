1edbf3b3eb48f87a990d1ca7caeb90cb678b7f0b
revione <revione@outlook.com>
2023-11-13 15:35:56 +0100

    cards animation

--- src/Sections/4/index.tsx
+++ src/Sections/4/index.tsx
@@ -2,31 +2,31 @@ import "./style.css";
 
 const CardsInfo = [
   {
-    imgSrc: "/section4/bg-1-section-4.png",
+    imgSrc: "/section4/section-4-1.png",
     title: "Tu luz adecuada",
     content:
       "Cuál es su fuente, cuál es la luz que ilumina tu vida, tu camino, tu viaje y te proporciona todo de manera divina y perfecta.",
   },
   {
-    imgSrc: "/section4/bg-2-section-4.png",
+    imgSrc: "/section4/section-4-2.png",
     title: "Tu agua de vida",
     content:
       "Es esencial en nuestra nutrición y vitalidad, en nuestra fluidez y adaptabilidad, en nuestra purificación y renovación, en nuestra fuerza y perseverancia.",
   },
   {
-    imgSrc: "/section4/bg-3-section-4.png",
+    imgSrc: "/section4/section-4-3.png",
     title: "Tu nueva humanidad",
     content:
       "En esta amplia gama de sensaciones, como esa luz que nos ilumina en todo momento, ese cuerpo que nos ha sido entregado para respetarlo, cuidarlo y protegerlo, esa agua de vida que consumimos y ese aire sano y limpio que respiramos para encontrar ese propósito de vida que nos lleve a ser nuestra mejor versión.",
   },
   {
-    imgSrc: "/section4/bg-4-section-4.png",
+    imgSrc: "/section4/section-4-4.png",
     title: "Tu cuerpo fértil",
     content:
       "La aceptación y la gratitud, junto con tu diálogo interno positivo, el autoconocimiento y el desarrollo de tu inteligencia emocional, son elementos clave que te impulsarán a un nivel superior en tu vida.",
   },
   {
-    imgSrc: "/section4/bg-5-section-4.png",
+    imgSrc: "/section4/section-4-5.png",
     title: "Tu aire nutritivo",
     content:
       "Identificar y aceptar el aire que nos nutre, que acompaña nuestro ser y nos permite respirar con paz y tranquilidad. Este entorno de relaciones significativas y sólidas.",
